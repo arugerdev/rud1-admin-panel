fea7c9872546bf057d78f9996767344f35787a6d
arugerdev <alvaroch250@gmail.com>
2025-03-10 20:06:11 +0100

    Optimize build timing

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  webpack(config, { isServer }) {
+    if (!isServer) {
+      config.cache = {
+        type: 'filesystem',
+      };
+    }
+    return config;
+  },
+
 };
 
 export default nextConfig;
